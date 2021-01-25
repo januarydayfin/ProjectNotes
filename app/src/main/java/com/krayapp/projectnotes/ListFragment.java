d1a2bd6e68c769cd70090d0b2d598871dd7350ad
krayman <50882897+krayman@users.noreply.github.com>
2021-01-25 19:03:40 +0700

    Первоначальный дизайн

--- app/src/main/java/com/krayapp/projectnotes/ListFragment.java
+++ app/src/main/java/com/krayapp/projectnotes/ListFragment.java
@@ -2,7 +2,9 @@ package com.krayapp.projectnotes;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,17 +13,23 @@ import android.view.ViewGroup;
 
 public class ListFragment extends Fragment {
 
-
-
+    private static final String KEY_MEMORY = "KEY_MEMORY";
     public ListFragment() {
         // Required empty public constructor
     }
 
-    public static ListFragment newInstance(String param1, String param2) {
+    public static ListFragment newInstance() {
         ListFragment fragment = new ListFragment();
         Bundle args = new Bundle();
         fragment.setArguments(args);
         return fragment;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        showFirst();
     }
 
     @Override
@@ -36,5 +44,9 @@ public class ListFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    private void showFirst(){
+        ListFragment listFragment = ListFragment.newInstance();
     }
 }
