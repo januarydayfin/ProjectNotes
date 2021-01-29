c3addda79aeb63915d2671ffea062c352518a322
krayman <50882897+krayman@users.noreply.github.com>
2021-01-29 18:30:12 +0700

    Сделал menu, сделал создание главного фрагмента через менеджера. Исправлен автоповорот

--- app/src/main/java/com/krayapp/projectnotes/MainActivity.java
+++ app/src/main/java/com/krayapp/projectnotes/MainActivity.java
@@ -1,14 +1,57 @@
 package com.krayapp.projectnotes;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.toolbar_main);
+        addMainFragment();
+        initToolbar();
+    }
+
+    private boolean checkLand() {
+        if (getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Здесь определяем меню приложения (активити)
+        Toast.makeText(this, "Menu IN", Toast.LENGTH_SHORT).show();
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    private void addMainFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        if (checkLand()) {
+            fragmentTransaction.replace(R.id.landNoteList, new ListFragment());
+        } else {
+            fragmentTransaction.replace(R.id.listNoteContainer, new ListFragment());
+        }
+        fragmentTransaction.commitAllowingStateLoss();
     }
 }
